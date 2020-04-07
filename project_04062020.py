8d6a85a981909581aa489ee67c6141ce74d6018f
Stephen Macke <smacke@illinois.edu>
2020-04-07 14:59:10 -0700

    fix possible bug

--- project_04062020.py
+++ project_04062020.py
@@ -10,7 +10,7 @@ class UNEXPECTED_STATES(Exception):
         Visit_node represents this error happened in which method(e.g. visit_Assign) of the ast.NodeVisitor
         error_node is the ast node argument that caused this error
         msg is the extra msg to explain the error"""
-    def __init__(stage, visit_node, error_node, msg = ""):
+    def __init__(self, state, visit_node, error_node, msg = ""):
         self.stage = state
         self.visit_node = visit_node
         self.error_node = error_node

089d2d10d4ecc29fe275e7137ca519f13f743b15
Stephen Macke <smacke@illinois.edu>
2020-04-14 22:43:18 -0700

    minimal working

--- nbsafety/safe_kernel/kernel.py
+++ nbsafety/safe_kernel/kernel.py
@@ -2,21 +2,15 @@ from ipykernel.ipkernel import IPythonKernel
 from ..version import __version__
 
 
-preamble = """
-import ....
-# set up data structures
-# whatever else is needed
-"""
 class SafeKernel(IPythonKernel):
     implementation = 'safe_kernel'
     implementation_version = __version__
 
     def __init__(self, **kwargs):
-        super(self.__class__, self).__init__(**kwargs)
+        super().__init__(**kwargs)
+        self.shell.run_cell('from nbsafety.project_04062020 import dependency_safety')
 
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
-        self.shell.run_cell('x=3+4', silent=True)
-        self.shell.run_cell('3+x')
-        reply = super(self.__class__, self).do_execute(code, silent, store_history, user_expressions, allow_stdin)
-        return reply
+        code = "%%dependency_safety\n" + code
+        return super().do_execute(code, silent, store_history, user_expressions, allow_stdin)

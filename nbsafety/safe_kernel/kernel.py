4cb30e132f20095014ea89c89295f90c3f602c0d
Stephen Macke <smacke@illinois.edu>
2020-04-18 09:43:04 -0700

    revert refactorability change

--- nbsafety/safe_kernel/kernel.py
+++ nbsafety/safe_kernel/kernel.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from ipykernel.ipkernel import IPythonKernel
-import nbsafety.safety
-from nbsafety.version import __version__
+# import nbsafety.safety
+# from nbsafety.safety import dependency_safety, dependency_safety_init
+from ..version import __version__
 
 
 class SafeKernel(IPythonKernel):
@@ -11,11 +12,16 @@ class SafeKernel(IPythonKernel):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
         self.shell.run_cell('from {} import {}'.format(
-            nbsafety.safety.__name__, nbsafety.safety.dependency_safety.__name__)
-        )
-        self.shell.run_cell('{}()'.format(nbsafety.safety.dependency_safety_init.__name__))
+            # nbsafety.safety.__name__, dependency_safety.__name__)
+            'nbsafety.safety', 'dependency_safety, dependency_safety_init'
+        ))
+        self.shell.run_cell('{}()'.format(
+            # dependency_safety_init.__name__
+            'dependency_safety_init'
+        ))
 
     def do_execute(self, code, silent, store_history=True,
                    user_expressions=None, allow_stdin=False):
-        code = "%%{}\n".format(nbsafety.dependency_safety.__name__) + code
+        # code = "%%{}\n".format(dependency_safety.__name__) + code
+        code = "%%{}\n{}".format('dependency_safety', code)
         return super().do_execute(code, silent, store_history, user_expressions, allow_stdin)

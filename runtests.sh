09107a4a6a8697671bc96b81587d63d3c0d7ea99
Stephen Macke <smacke@illinois.edu>
2020-04-20 14:23:39 -0700

    use simple ipython prompt for test running

--- runtests.sh
+++ runtests.sh
@@ -1,3 +1,4 @@
 #!/usr/bin/env bash
 
-ipython3 tests/test_runner.py -- $@
+# ref: https://github.com/ipython/ipython/issues/9752
+ipython3 --quick --no-banner --quiet --colors=NoColor --simple-prompt tests/test_runner.py -- $@

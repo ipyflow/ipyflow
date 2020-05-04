7d7eecf7bfef3a02881932f3aff7a83ae6049dfe
Ray Gong <ruiduoray@berkeley.edu>
2020-05-03 19:25:52 -0700

    Added usage explanation for line magic function

--- nbsafety/safety.py
+++ nbsafety/safety.py
@@ -38,11 +38,7 @@ class DependencySafety(object):
         self._last_refused_code: Optional[str] = None
         self._last_cell_ast: Optional[ast.Module] = None
         self._track_dependencies = True
-        """level 0: Warning,    Stop Code,   Record new dependencies, (Full functionality)
-           level 1: Warning,    Run code,    Record new dependencies, (Don't stop at the warning)
-           level 2: No Warning, Run code,    Record new dependencies, (Don't show warnings)
-           level 3: No warning, Run cocde,   No new dependencies,     (Original Kernel)
-        """
+
         self._disable_level = 0
 
     def _make_cell_magic(self, cell_magic_name):
@@ -99,7 +95,38 @@ class DependencySafety(object):
     def _make_line_magic(self, line_magic_name):
         def _safety(line: str):
             line = line.split()
-            if not line:
+            if not line or line[0] not in ["show_graph", "show_dependency", "show_stale", "set_disable_level",
+                                            "remove_dependency", "add_dependency", "turn_off_warnings_for", "turn_on_warnings_for"]:
+                print("""
+Options:
+
+show_graph: 
+    -This will print out the dependency graph of global variables. Stale nodes are labeled red. Notice that user might need to call this twice to have it to work.
+
+show_dependency <variable_name> <variable_name2> ...: 
+    -This will print out the dependencies for given global vriables. Multiple variables should be seperated with spaces.
+
+show_stale: 
+    -This will print out all the global variables that are stale. 
+
+set_disable_level <integer>:
+    -level 0: Warning,    Stop Code,   Record new dependencies, (Full functionality)
+    -level 1: Warning,    Run code,    Record new dependencies, (Don't stop at the warning)
+    -level 2: No Warning, Run code,    Record new dependencies, (Don't show warnings)
+    -level 3: No warning, Run cocde,   No new dependencies,     (Original Kernel)
+
+remove_dependency <parent_name> <child_name>:
+    -This will remove the dependency between parent variable and the child variable.
+
+add_dependency <parent_name> <child_name>:
+    -This will add the dependency between parent variable and the child variable.
+
+turn_off_warnings_for  <variable_name> <variable_name2> ...: 
+    -This will turn off the warnings for given global variables. These variables will never be considered as stale anymore. Multiple variables should be seperated with spaces.
+
+
+turn_off_warnings_for  <variable_name> <variable_name2> ...: 
+    -This will turn the warnings back on for given global variables. These variables could have a stale dependency now. Multiple variables should be seperated with spaces.""")
                 return
             if line[0] == "show_graph":
                 graph = nx.DiGraph()
@@ -141,10 +168,10 @@ class DependencySafety(object):
             elif line[0] == "set_disable_level":
                 if len(line) != 2 or line[1] not in ['0','1','2','3']:
                     print("""Usage: %safety set_disable_level <integer>
-                        level 0: Warning,    Stop Code,   Record new dependencies, (Full functionality)
-                        level 1: Warning,    Run code,    Record new dependencies, (Don't stop at the warning)
-                        level 2: No Warning, Run code,    Record new dependencies, (Don't show warnings)
-                        level 3: No warning, Run cocde,   No new dependencies,     (Original Kernel)""")
+-level 0: Warning,    Stop Code,   Record new dependencies, (Full functionality)
+-level 1: Warning,    Run code,    Record new dependencies, (Don't stop at the warning)
+-level 2: No Warning, Run code,    Record new dependencies, (Don't show warnings)
+-level 3: No warning, Run cocde,   No new dependencies,     (Original Kernel)""")
                     return
                 self._disable_level = int(line[1])
             elif line[0] == "remove_dependency":

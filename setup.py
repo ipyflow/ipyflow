ecca625ca77f741249b5d7a379d422644a9a971e
Stephen Macke <stephen.macke@gmail.com>
2020-05-27 22:44:00 -0700

    fix tests to get return code and remove python 3.6 from setup.py

--- setup.py
+++ setup.py
@@ -47,7 +47,6 @@ setup(
         'Intended Audience :: Developers',
         'License :: OSI Approved :: BSD License',
         'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
 )
--- test_runner.py
+++ test_runner.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 # need PYTHONPATH="." for this to work
+import builtins
 try:
     import test
 except ImportError:
@@ -9,9 +10,16 @@ except ImportError:
     import test
 
 if __name__ == '__main__':
+    setattr(builtins, '__exit_zero', True)
     for name, mod in test.__dict__.items():
         # weird; need to reimport these for some reason
         import sys
         import ipytest
         if name.startswith('test_') and isinstance(mod, type(sys)):
-            ipytest.run(*sys.argv[1:], filename=mod.__file__)
+            if ipytest.run(*sys.argv[1:], filename=mod.__file__, return_exit_code=True) != 0:
+                import builtins
+                setattr(builtins, '__exit_zero', False)
+    # Totally bizarre; we lose all our variables
+    # Extreme hack to keep the zero exit status around
+    import builtins
+    sys.exit(0) if getattr(builtins, '__exit_zero') else sys.exit(1)

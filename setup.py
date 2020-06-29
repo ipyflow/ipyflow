12f2c56a7c7a3bc0fe8408c24ba71d8da64bdfc8
Stephen Macke <stephen.macke@gmail.com>
2020-06-28 22:13:27 -0700

    try to compute kernelspec json in setup.py

--- setup.py
+++ setup.py
@@ -1,24 +1,25 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-import atexit
-import platform
-from subprocess import check_call
-
+import json
+import os
+import sys
 from setuptools import setup, find_packages
+
+from IPython.utils.tempdir import TemporaryDirectory
 import versioneer
 
 pkg_name = 'nbsafety'
 
-
-def _post_install_hook():
-    python_command_suffix = ''
-    if platform.system().lower().startswith('win'):
-        python_command_suffix = '.exe'
-    check_call(f'python{python_command_suffix} -m {pkg_name}.install'.split())
-
-
-atexit.register(_post_install_hook)
+DISPLAY_NAME = f"Python 3 ({pkg_name})"
+KERNEL_JSON = {
+    "argv": [
+        sys.executable, "-m", "nbsafety.kernel", "-f", "{connection_file}",
+    ],
+    "display_name": DISPLAY_NAME,
+    "language": "python",
+    "codemirror_mode": "shell",
+}
 
 
 def read_file(fname):
@@ -28,52 +29,71 @@ def read_file(fname):
 
 history = read_file('HISTORY.rst')
 requirements = read_file('requirements.txt').strip().split()
-setup(
-    name=pkg_name,
-    version=versioneer.get_version(),
-    cmdclass=versioneer.get_cmdclass(),
-    author='Stephen Macke',
-    author_email='stephen.macke@gmail.com',
-    description='Fearless interactivity for Jupyter notebooks.',
-    long_description=read_file('README.md'),
-    long_description_content_type='text/markdown',
-    url='https://github.com/nbsafety-project/nbsafety',
-    packages=find_packages(exclude=[
-        'binder',
-        'docs',
-        'scratchspace',
-        'notebooks',
-        'img',
-        'test',
-        'scripts',
-        'markdown',
-        'versioneer.py',
-        'frontend',
-        'blueprint.json',
-    ]),
-    include_package_data=True,
-    data_files=[
-        # like `jupyter nbextension install --sys-prefix`
-        ("share/jupyter/nbextensions/nbsafety", [
-            "nbsafety/resources/nbextension/index.js",
+
+with TemporaryDirectory() as td:
+    os.chmod(td, 0o755)  # Starts off as 700, not user readable
+    kernel_json_file = os.path.join(td, 'kernel.json')
+    with open(kernel_json_file, 'w') as f:
+        json.dump(KERNEL_JSON, f, sort_keys=True)
+    setup(
+        name=pkg_name,
+        version=versioneer.get_version(),
+        cmdclass=versioneer.get_cmdclass(),
+        author='Stephen Macke',
+        author_email='stephen.macke@gmail.com',
+        description='Fearless interactivity for Jupyter notebooks.',
+        long_description=read_file('README.md'),
+        long_description_content_type='text/markdown',
+        url='https://github.com/nbsafety-project/nbsafety',
+        packages=find_packages(exclude=[
+            'binder',
+            'docs',
+            'scratchspace',
+            'notebooks',
+            'img',
+            'test',
+            'scripts',
+            'markdown',
+            'versioneer.py',
+            'frontend',
+            'blueprint.json',
         ]),
-        # like `jupyter nbextension enable --sys-prefix`
-        ("etc/jupyter/nbconfig/notebook.d", [
-            "nbsafety/resources/nbextension/nbsafety.json",
-        ])
-    ],
-    install_requires=requirements,
-    license='BSD-3-Clause',
-    zip_safe=False,
-    classifiers=[
-        'Development Status :: 3 - Alpha',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
-        'Natural Language :: English',
-        'Programming Language :: Python :: 3.6',
-        'Programming Language :: Python :: 3.7',
-    ],
-)
+        include_package_data=True,
+        data_files=[
+            # like `jupyter nbextension install --sys-prefix`
+            ("share/jupyter/nbextensions/nbsafety", [
+                "nbsafety/resources/nbextension/index.js",
+            ]),
+            ("share/jupyter/nbextensions/nbsafety", [
+                "nbsafety/resources/nbextension/index.js.map",
+            ]),
+            # like `jupyter nbextension enable --sys-prefix`
+            ("etc/jupyter/nbconfig/notebook.d", [
+                "nbsafety/resources/nbextension/nbsafety.json",
+            ]),
+            # like `python -m nbsafety.install --sys-prefix`
+            ("share/jupyter/kernels/nbsafety", [
+                kernel_json_file,
+            ]),
+            ("share/jupyter/kernels/nbsafety", [
+                "nbsafety/resources/img/logo-32x32.png",
+            ]),
+            ("share/jupyter/kernels/nbsafety", [
+                "nbsafety/resources/img/logo-64x64.png",
+            ]),
+        ],
+        install_requires=requirements,
+        license='BSD-3-Clause',
+        zip_safe=False,
+        classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'License :: OSI Approved :: BSD License',
+            'Natural Language :: English',
+            'Programming Language :: Python :: 3.6',
+            'Programming Language :: Python :: 3.7',
+        ],
+    )
 
 # python setup.py sdist bdist_wheel --universal
 # twine upload dist/*

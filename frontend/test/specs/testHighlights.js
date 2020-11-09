afe0e7f16a461532ff83f62e8d506fe3b67b7162
Stephen Macke <stephen.macke@gmail.com>
2020-11-09 09:08:07 -0800

    merge spec files for ui tests

---
+++ frontend/test/specs/testHighlights.js
@@ -0,0 +1,62 @@
+
+var NbsafetyPageObject = require('../src/nbsafety.po.js');
+var nbsafetyPO;
+
+describe('(Python) cell highlights', function () {
+    beforeEach(function () {
+        nbsafetyPO = new NbsafetyPageObject();
+    });
+
+    afterEach(function () {
+        nbsafetyPO.closeAndHaltNotebook();
+    });
+
+    describe('Dependency mutated', function () {
+        it('Should highlight stale input / output cells after dependency mutated', function () {
+            nbsafetyPO.runNotebookByUrl('/test/notebooks/simplest.ipynb');
+            nbsafetyPO.runCodeCellByIndex(0);
+            var refresherCodeCell = nbsafetyPO.runCodeCellByIndex(1);
+            var printCodeCell = nbsafetyPO.runCodeCellByIndex(2);
+            nbsafetyPO.runCodeCellByIndex(3);
+            expect(refresherCodeCell).toHaveClass('refresher-input-cell');
+            expect(refresherCodeCell).toHaveClass('stale-output-cell');
+            expect(printCodeCell).toHaveClass('stale-cell');
+            expect(printCodeCell).toHaveClass('stale-output-cell');
+
+            nbsafetyPO.runCodeCellByIndex(1);
+            expect(refresherCodeCell).not.toHaveClass('refresher-input-cell');
+            expect(refresherCodeCell).not.toHaveClass('stale-output-cell');
+            expect(printCodeCell).not.toHaveClass('stale-cell');
+            expect(printCodeCell).toHaveClass('stale-output-cell');
+
+            nbsafetyPO.runCodeCellByIndex(2);
+            expect(printCodeCell).not.toHaveClass('stale-cell');
+            expect(printCodeCell).not.toHaveClass('stale-output-cell');
+        });
+    });
+
+    describe('Hover over highlight', function () {
+        it('Should highlight linked stale / refresher cell(s) when hovering over a cell highlight', function () {
+            nbsafetyPO.runNotebookByUrl('/test/notebooks/chain.ipynb');
+            nbsafetyPO.runCodeCellByIndex(0);
+            var refresherCodeCell = nbsafetyPO.runCodeCellByIndex(1);
+            nbsafetyPO.runCodeCellByIndex(2);
+            nbsafetyPO.runCodeCellByIndex(3);
+            var lastInChain = nbsafetyPO.runCodeCellByIndex(4);
+            nbsafetyPO.runCodeCellByIndex(5);
+            expect(refresherCodeCell).toHaveClass('refresher-input-cell');
+            expect(refresherCodeCell).toHaveClass('stale-output-cell');
+            expect(lastInChain).toHaveClass('stale-cell');
+            expect(lastInChain).toHaveClass('stale-output-cell');
+
+            var linkedStale = nbsafetyPO.getInputCollapserChildByIndex(4);
+            var linkedRefresher = nbsafetyPO.getInputCollapserChildByIndex(1);
+            linkedStale.moveTo();
+            expect(linkedRefresher).toHaveClass('linked-refresher');
+            browser.pause(500);
+            linkedRefresher.moveTo();
+            expect(linkedStale).toHaveClass('linked-stale');
+            browser.pause(500);
+        });
+    });
+});

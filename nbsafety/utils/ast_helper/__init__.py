06a6afec3e700bdc5c3c160e502db730f02d5e04
Stephen Macke <stephen.macke@gmail.com>
2020-12-28 17:08:56 -0800

    still working on python 3.6 issues

--- nbsafety/utils/ast_helper/__init__.py
+++ nbsafety/utils/ast_helper/__init__.py
@@ -4,4 +4,7 @@ import sys
 if sys.version_info >= (3, 7):
     from . import ast_helper as fast
 else:
+    from typing import cast
+    from . import ast_helper
     from .ast_helper import FastAst as fast
+    fast = cast(type(ast_helper), fast)

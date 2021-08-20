c6039955220baa607cbd482005d75b501ba17003
Stephen Macke <stephen.macke@gmail.com>
2021-08-20 00:11:29 -0700

    use yarn install from lockfile instead of npm ci

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,10 +3,10 @@
 mkdir -p ./nbsafety/resources/labextension
 mkdir -p ./nbsafety/resources/nbextension
 pushd ./frontend/labextension
-npm ci && npm run build:prod
+yarn install --frozen-lockfile && npm run build:prod
 popd
 pushd ./frontend/nbextension
-npm ci && npm run build
+yarn install --frozen-lockfile && npm run build
 popd
 cp ./frontend/labextension/install.json ./nbsafety/resources/labextension
 cp ./frontend/nbextension/nbsafety.json ./nbsafety/resources/nbextension

ffe5f3731aea4b3d2aae6848760f740c0ee5f012
Stephen Macke <stephen.macke@gmail.com>
2021-09-01 17:48:08 -0700

    fix extension link script if non-dev version already installed

--- scripts/extlink.sh
+++ scripts/extlink.sh
@@ -3,9 +3,15 @@
 PREFIX="$(python -c 'import sys; print(sys.prefix)')"
 mkdir -p "${PREFIX}"/share/jupyter/labextensions
 pushd "${PREFIX}"/share/jupyter/labextensions
+if [[ -d jupyterlab-nbsafety ]]; then
+    rm -rf jupyterlab-nbsafety
+fi
 ln -s -f "$(dirs -l -p | tail -1)"/nbsafety/resources/labextension jupyterlab-nbsafety
 popd
-pushd "${PREFIX}"/share/jupyter/nbextensions
 mkdir -p "${PREFIX}"/share/jupyter/nbextensions
+pushd "${PREFIX}"/share/jupyter/nbextensions
+if [[ -d nbsafety ]]; then
+    rm -rf nbsafety
+fi
 ln -s -f "$(dirs -l -p | tail -1)"/nbsafety/resources/nbextension nbsafety
 popd

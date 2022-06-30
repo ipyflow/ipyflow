fbd4a5f7ba9659965bf1a1aee49c20c3ff0d9ee1
Stephen Macke <stephen.macke@gmail.com>
2022-06-30 16:57:15 -0700

    nvm use default in build script, actually fix version stuff

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,11 @@
-#!/usr/bin/env bash
+#!/usr/bin/env zsh
+
+set -e  # just use set -e while we source nvm
+source $HOME/.nvm/nvm.sh;
+nvm use default
+
+# ref: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -euxo pipefail
 
 mkdir -p ./core/ipyflow/resources/labextension
 mkdir -p ./core/ipyflow/resources/nbextension

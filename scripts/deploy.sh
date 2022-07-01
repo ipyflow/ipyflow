0b07cdc8bb3c571ff2228c46a8c3758cded3800d
Stephen Macke <stephen.macke@gmail.com>
2022-06-30 17:56:33 -0700

    activate nvm in deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+set -e  # just use set -e while we source nvm
+source $HOME/.nvm/nvm.sh;
+nvm use default
+
 # ref: https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
 set -euxo pipefail
 

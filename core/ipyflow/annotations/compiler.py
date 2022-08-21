68192db21d0a3f17cb3a3ec7edf5eaf75ef9243d
Stephen Macke <stephen.macke@gmail.com>
2022-08-21 15:20:05 -0400

    working on DSL compiler scaffolding

--- core/ipyflow/annotations/compiler.py
+++ core/ipyflow/annotations/compiler.py
@@ -4,12 +4,14 @@ Compiles the annotations in .pyi files into handlers for library code.
 """
 import ast
 from collections import defaultdict
-from typing import Dict, List, Tuple
+from typing import Dict, List, Tuple, Type
+
+from ipflow.utils.ast_utils import subscript_to_slice
 
 from ipyflow.tracing.external_call_handler import ExternalCallHandler, NoopCallHandler
 
 
-def compile_function_handler(func: ast.FunctionDef) -> ExternalCallHandler:
+def compile_function_handler(func: ast.FunctionDef) -> Type[ExternalCallHandler]:
     # step 1: union arguments into groups such that any 2 args reference the same symbol somehow
     # step 2: for each group, create a handler that searches for a solution to the group constraint
     # step 3: combine the handlers into a single handler
@@ -20,18 +22,28 @@ def compile_function_handler(func: ast.FunctionDef) -> ExternalCallHandler:
         )
     if isinstance(ret, ast.Name):
         if ret.id == "NoopCallHandler":
-            return NoopCallHandler()
+            return NoopCallHandler
         else:
             raise ValueError(f"No known handler for return type {ret}")
     elif isinstance(ret, ast.Subscript):
-        pass
+        sub_value = ret.value
+        slice_value = subscript_to_slice(ret)
+        if isinstance(sub_value, ast.Name):
+            if sub_value.id == "Mutated":
+                if isinstance(slice_value, ast.Name):
+                    if slice_value.id == "__module__":
+                        raise ValueError(f"No known handler for return type {ret}")
+                    else:
+                        raise ValueError(f"No known handler for return type {ret}")
+            else:
+                raise ValueError(f"No known handler for return type {ret}")
     else:
         raise TypeError(
             f"unable to handle return type {ret} when trying to compile {func.name}"
         )
 
 
-def compile_class_handler(cls: ast.ClassDef) -> List[ExternalCallHandler]:
+def compile_class_handler(cls: ast.ClassDef) -> List[Type[ExternalCallHandler]]:
     handlers = []
     for func in cls.body:
         if not isinstance(func, ast.FunctionDef):
@@ -86,14 +98,16 @@ def get_specs_by_module(
 
 def compile_classes(
     classes: List[ast.ClassDef],
-) -> Dict[str, List[ExternalCallHandler]]:
+) -> Dict[str, List[Type[ExternalCallHandler]]]:
     handlers_by_class = {}
     for clazz in classes:
         handlers_by_class[clazz.name] = compile_class_handler(clazz)
     return handlers_by_class
 
 
-def compile_functions(functions: List[ast.FunctionDef]) -> List[ExternalCallHandler]:
+def compile_functions(
+    functions: List[ast.FunctionDef],
+) -> List[Type[ExternalCallHandler]]:
     function_handlers = []
     for func in functions:
         function_handlers[module].append(compile_function_handler(func))

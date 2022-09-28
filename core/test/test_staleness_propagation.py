1fca8e4a2f1428b258838445fa3ccbe5100bea8e
Stephen Macke <stephen.macke@gmail.com>
2022-09-27 23:28:54 -0700

    test

--- core/test/test_staleness_propagation.py
+++ core/test/test_staleness_propagation.py
@@ -58,6 +58,7 @@ def test_simplest():
     run_cell("a = 3")
     run_cell("logging.info(b)")
     assert_detected("should have detected b has stale dep on old a")
+    assert False
 
 
 def test_readme_example():

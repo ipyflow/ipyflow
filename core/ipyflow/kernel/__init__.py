0445e71683f011277f12e9e87ff490c5c7c3f740
Stephen Macke <stephen.macke@gmail.com>
2022-11-23 11:34:25 -0800

    improved gc thresholds

--- core/ipyflow/kernel/__init__.py
+++ core/ipyflow/kernel/__init__.py
@@ -3,3 +3,22 @@
 from ipyflow.kernel.kernel import IPyflowKernel
 
 __all__ = ["IPyflowKernel"]
+
+
+# ref: https://mkennedy.codes/posts/python-gc-settings-change-this-and-make-your-app-go-20pc-faster/
+def _set_gc_thresholds():
+    import gc
+
+    # Clean up what might be garbage so far.
+    gc.collect(2)
+    # Exclude current items from future GC.
+    gc.freeze()
+
+    allocs, gen1, gen2 = gc.get_threshold()
+    allocs = 50_000  # Start the GC sequence every 50K not 700 allocations.
+    gen1 = gen1 * 2
+    gen2 = gen2 * 2
+    gc.set_threshold(allocs, gen1, gen2)
+
+
+_set_gc_thresholds()

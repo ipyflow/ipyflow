d41f32e1d8cdd882b72af173764f570b8666bfdb
Stephen Macke <stephen.macke@gmail.com>
2024-07-19 17:24:26 -0700

    fix ast.Module incompatibility

--- core/ipyflow/analysis/live_refs.py
+++ core/ipyflow/analysis/live_refs.py
@@ -626,7 +626,10 @@ def compute_live_dead_symbol_refs(
     if isinstance(code, str):
         code = ast.parse(code)
     elif isinstance(code, list):
-        code = ast.Module(code, [])
+        if sys.version_info < (3, 8):
+            code = ast.Module(code)
+        else:
+            code = ast.Module(code, [])
     return ComputeLiveSymbolRefs(
         scope=scope, init_killed=init_killed, include_killed_live=include_killed_live
     )(code)
